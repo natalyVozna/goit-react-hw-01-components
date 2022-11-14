664a199865234f6965ae728a86532d77037b3dc5
natalyVozna <109243941+natalyVozna@users.noreply.github.com>
2022-11-14 14:54:08 +0200

    theme

---
+++ src/constans/theme.js
@@ -0,0 +1,12 @@
+export const theme = Object.freeze({
+  colors: {
+    white: '#ffffff',
+    praimeryText: '#2c2c2c',
+    text: '#212121',
+    accent: 'rgb(33, 148, 242)',
+    gray: '#afb1b8',
+    primaryBg: '#f5f5f5',
+    secondaryBg: '#f5f4fa',
+    timingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+});
